b1720b7f3ff7e67585d68a25156074370353d12c
Terez223 <114877792+Terez223@users.noreply.github.com>
2022-10-31 21:44:06 +0100

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,49 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    strip.show()
-    strip2.showColor(neopixel.colors(NeoPixelColors.Red))
-    strip2.show()
-    strip3.showColor(neopixel.colors(NeoPixelColors.Red))
-    strip3.show()
-})
-input.onButtonPressed(Button.AB, function () {
-    strip.showRainbow(1, 360)
-    strip.rotate(1)
-    strip.show()
-    strip2.showRainbow(1, 360)
-    strip2.rotate(1)
-    strip2.show()
-    strip3.showRainbow(1, 360)
-    strip3.rotate(1)
-    strip3.show()
+    for (let index = 0; index < 10; index++) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        basic.pause(100)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        basic.pause(100)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-    strip2.showColor(neopixel.colors(NeoPixelColors.Yellow))
-    strip3.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
     strip.show()
 })
-let strip3: neopixel.Strip = null
-let strip2: neopixel.Strip = null
 let strip: neopixel.Strip = null
-basic.showLeds(`
-    . # # # .
-    . # . # .
-    # . # . #
-    . # . # .
-    . # # # .
-    `)
-strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P1, 30, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
-strip2 = neopixel.create(DigitalPin.P1, 30, NeoPixelMode.RGB)
-strip2.showRainbow(1, 360)
-strip3 = neopixel.create(DigitalPin.P2, 30, NeoPixelMode.RGB)
-strip3.showRainbow(1, 360)
 basic.forever(function () {
     strip.rotate(1)
     strip.show()
-    strip2.rotate(1)
-    strip2.show()
-    strip3.rotate(1)
-    strip3.show()
 })
