ebf2a4f181b8491a9c997674eee38b1e7cdc78c1
Terez223 <114877792+Terez223@users.noreply.github.com>
2022-10-31 20:20:05 +0100

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
 basic.showLeds(`
-    . # . # .
-    . . . . .
-    # . . . #
     . # # # .
-    . . . . .
+    . # # # .
+    # # # # #
+    . # # # .
+    . # # # .
     `)
 let strip = neopixel.create(DigitalPin.P0, 20, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
+let strip2 = neopixel.create(DigitalPin.P1, 30, NeoPixelMode.RGB)
+strip2.showRainbow(1, 360)
+let strip3 = neopixel.create(DigitalPin.P2, 30, NeoPixelMode.RGB)
+strip3.showRainbow(1, 360)
 basic.forever(function () {
     strip.rotate(1)
     strip.show()
+    strip2.rotate(1)
+    strip2.show()
+    strip3.rotate(1)
+    strip3.show()
 })
